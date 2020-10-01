9630fcc2f5e8b16034cd7b318bf8844f6f224788
oussama <oussama.ghzl6@gmail.com>
2020-10-01 14:39:44 +0200

    Calculatrice presque fini manque assignation des touches

--- js/module/calculatrice.js
+++ js/module/calculatrice.js
@@ -35,6 +35,82 @@ let tab2 = [].slice.call(row2.children)
 let tab3 = [].slice.call(row3.children)
 let tab4 = [].slice.call(row4.children)
 
+let bouton = [btn,
+    btn1,
+    btn2,
+    btn3,
+    btn4,
+    btn5,
+    btn6,
+    btn7,
+    btn8,
+    btn9,
+    btn10,
+    btn11,
+    btn12,
+    btn13,
+    btn14,
+    btn15
+]
+bouton.forEach(element => {
+    element.addEventListener('click',(e)=>{
+        if (e.target.innerText == 'C') {
+            result = 0;
+            myReponse.textContent = 'Resultat';
+            myInput.value = ''
+    
+        } else if (e.target.innerText == '=') {
+            nombre2 = parseInt(calcul);
+            myInput.value = ''
+            if (operateur == '+') {
+                result = nombre1 + nombre2
+                calcul = ''
+                myReponse.textContent = result
+            } else if (operateur == '-') {
+                result = nombre1 - nombre2
+                calcul = ''
+                myReponse.textContent = result
+            } else if (operateur == '/') {
+                result = nombre1 / nombre2
+                calcul = ''
+                myReponse.textContent = result
+            } else if (operateur == '*') {
+                result = nombre1 * nombre2
+                calcul = ''
+                myReponse.textContent = result
+            }
+    
+        } else if (e.target.innerText == '+') {
+            operateur = '+'
+            nombre1 = parseInt(calcul);
+            calcul = '';
+            myInput.value = ''
+        } else if (e.target.innerText == '-') {
+            operateur = '-'
+            nombre1 = parseInt(calcul);
+            calcul = '';
+            myInput.value = ''
+    
+        } else if (e.target.innerText == '/') {
+            operateur = '/'
+            nombre1 = parseInt(calcul);
+            calcul = '';
+            myInput.value = ''
+    
+        } else if (e.target.innerText == '*') {
+            operateur = '*'
+            nombre1 = parseInt(calcul);
+            calcul = '';
+            myInput.value = ''
+    
+        } else {
+            result = 0
+            myInput.value += e.target.innerText;
+            calcul += e.target.innerText;
+            myReponse.textContent = 'Resultat';
+        }
+    })
+});
 
 let result = 0;
 let calcul = '';
@@ -42,62 +118,3 @@ let nombre1;
 let nombre2;
 let operateur;
 
-myDiv2.addEventListener('click', (e) => {
-
-    if (e.target.innerText == 'C') {
-        result = 0;
-        myReponse.textContent = 'Resultat';
-        myInput.value = ''
-
-    } else if (e.target.innerText == '=') {
-        nombre2 = parseInt(calcul);
-        myInput.value = ''
-        if (operateur == '+') {
-            result = nombre1 + nombre2
-            calcul = ''
-            myReponse.textContent = result
-        } else if (operateur == '-') {
-            result = nombre1 - nombre2
-            calcul = ''
-            myReponse.textContent = result
-        } else if (operateur == '/') {
-            result = nombre1 / nombre2
-            calcul = ''
-            myReponse.textContent = result
-        } else if (operateur == '*') {
-            result = nombre1 * nombre2
-            calcul = ''
-            myReponse.textContent = result
-        }
-
-    } else if (e.target.innerText == '+') {
-        operateur = '+'
-        nombre1 = parseInt(calcul);
-        calcul = '';
-        myInput.value = ''
-    } else if (e.target.innerText == '-') {
-        operateur = '-'
-        nombre1 = parseInt(calcul);
-        calcul = '';
-        myInput.value = ''
-
-    } else if (e.target.innerText == '/') {
-        operateur = '/'
-        nombre1 = parseInt(calcul);
-        calcul = '';
-        myInput.value = ''
-
-    } else if (e.target.innerText == '*') {
-        operateur = '*'
-        nombre1 = parseInt(calcul);
-        calcul = '';
-        myInput.value = ''
-
-    } else {
-        result = 0
-        myInput.value += e.target.innerText;
-        calcul += e.target.innerText;
-        myReponse.textContent = 'Resultat';
-    }
-
-})
